9d216f72a0cf887b59c70a23d68a2a9776d7810e
Ireme-Nyawe <akimana.inono@gmail.com>
2024-07-13 07:45:20 +0200

    setting up basic structure for page

---
+++ src/components/CakeContainer.jsx
@@ -0,0 +1,9 @@
+function CakeContainer(){
+    return (
+        <div>
+            <h2>Number of cake</h2>
+            <button>buy a cake</button>
+        </div>
+    )
+}
+export default CakeContainer;
