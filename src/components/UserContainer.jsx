1710ba175e26e2da25fa444da49405f61caa72e7
Ireme-Nyawe <akimana.inono@gmail.com>
2024-07-15 19:36:40 +0200

    display users fetched

--- src/components/UserContainer.jsx
+++ src/components/UserContainer.jsx
@@ -1,4 +1,25 @@
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { fetchUsers } from "../redux";
 function UserContainer() {
-  return <div>users</div>;
+  const dispatch = useDispatch();
+  const users = useSelector((state) => {
+    return state.user;
+  });
+  console.log("user");
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, []);
+  return (
+    <div>
+      users
+      <hr />
+      <p>
+        {users.users.map((user) => (
+          <h2>{user.name}</h2>
+        ))}
+      </p>
+    </div>
+  );
 }
 export default UserContainer;
