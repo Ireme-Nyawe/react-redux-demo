cd21be8da0143328c33602872f20ffcf5798e453
Ireme-Nyawe <akimana.inono@gmail.com>
2024-07-15 08:40:06 +0200

    add logger midle ware

--- src/redux/Store.jsx
+++ src/redux/Store.jsx
@@ -1,6 +1,7 @@
-import { createStore } from "redux";
+import { createStore,applyMiddleware } from "redux";
+import logger from "redux-logger";
 import RootReducer from "./rootReducer";
 
-const Store = createStore(RootReducer);
+const Store = createStore(RootReducer,applyMiddleware(logger));
 
 export default Store;
