7173dcac3cd23accbf5c4a55acfceb4b1adf3bad
Ireme-Nyawe <akimana.inono@gmail.com>
2024-07-15 09:12:30 +0200

    add state variable to hold number

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Provider } from "react-redux";
 import Store from "./redux/Store";
 import { HooksCakeContainer } from "./components/HooksCakeContainer";
 import IceContainer from "./components/IceContainer";
+import NewCakeContainer from "./components/NewCakeContainer";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <CakeContainer />
       <HooksCakeContainer></HooksCakeContainer>
       <IceContainer></IceContainer>
+      <NewCakeContainer></NewCakeContainer>
     </Provider>
   );
 }
--- src/components/NewCakeContainer.jsx
+++ src/components/NewCakeContainer.jsx
@@ -1,3 +1,20 @@
-function NewCakeContainer(){
-    
+import { useSelector, useDispatch } from "react-redux";
+import { BuyCake } from "../redux";
+import { useState } from "react";
+function NewCakeContainer() {
+  const numOfCakes = useSelector((state) => state.cake.numOfCakes);
+  const dispatch = useDispatch();
+  const [number, setNumber] = useState(1);
+  return (
+    <div>
+      <h2>Number of cakes {numOfCakes}</h2>
+      <input
+        type="text"
+        value={number}
+        onChange={(e) => setNumber(e.target.value)}
+      />
+      <button onClick={() => dispatch(BuyCake())}>buy {number} cake</button>
+    </div>
+  );
 }
+export default NewCakeContainer;
